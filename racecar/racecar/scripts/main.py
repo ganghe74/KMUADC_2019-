5fa45c00ea502b82c7aa408dbe030c739282a2fe
FOSCAR <2014foscar@gmail.com>
2019-10-05 15:37:19 +0900

    comment test code

--- racecar/racecar/scripts/main.py
+++ racecar/racecar/scripts/main.py
@@ -171,8 +171,6 @@ def main():
                     time.sleep(0.05)
           
                 
-                
-                
                 #for i in range(5):
                 #    auto_drive(0.12, 2)
                 #    time.sleep(0.1)
@@ -188,15 +186,17 @@ def main():
             MODE = 0
             curve_detector.curve_count = 2
             obstacle_count += 1
-                          
+        
+                         
         #if obstacle_count == 4: 
         #    print(x_location) # for test code
         if x_location != None:
-            # test 3 lines
-            if obstacle_count == 4 and np.abs(x_location - x_location_old) < 40:
-                x_location = x_location_old
-            else:
-                x_location_old = x_location
+            # test 4 lines
+            #if obstacle_count == 4 and np.abs(x_location - x_location_old) < 40:
+            #    x_location = x_location_old
+            #else:
+            #    x_location_old = x_location
+            x_location_old = x_location
             pid = round(pidcal.pid_control(int(x_location),curve_detector.curve_count), 6)
             #print pid
             auto_drive(pid, curve_detector.curve_count, stop_counter.cnt,obstacle_count)
@@ -222,7 +222,7 @@ def main():
             obstacle_count = 0
             curve_detector.curve_count = 0
             car_run_speed = 2.0
-            print("--------------")
+
         if stop_counter.cnt == 3: # finish
             while 1:
                 auto_drive(0,0,3)
